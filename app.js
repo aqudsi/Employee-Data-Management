ca9e23eff581f5b2069604ef37ac174fcf3b3078
aqudsi <ahmerqudsi@gmail.com>
2017-08-08 15:09:48 -0400

    final commit

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
-$("#btn-submit").on("click", function(event){
-	event.preventDefault();
 
-	var employeeName = $("#employee-name").val().trim();
-	var role = $("#role").val().trim();
-	var startYear = $("#start-year").val().trim();
-	var salary = $("#salary").val().trim();
+  var config = {
+    apiKey: "AIzaSyAJ3FMcY2HzfbleApLzUTA2feFIOfL-Wvs",
+    authDomain: "ahmer-and-dains-project.firebaseapp.com",
+    databaseURL: "https://ahmer-and-dains-project.firebaseio.com",
+    projectId: "ahmer-and-dains-project",
+    storageBucket: "ahmer-and-dains-project.appspot.com",
+    messagingSenderId: "409363391438"
+  };
+  firebase.initializeApp(config);
+	var employeeName;
+	var role;
+	var startYear;
+	var salary;
+	var totalSalary;
+
+  var database = firebase.database();
+
+  // database.ref().on("value", function(snapshot){
+
+  // 	employeeName = snapshot.val().employeeName;
+  // 	role = snapshot.val().role;
+  // 	startYear = snapshot.val().startYear;
+  // 	salary = snapshot.val().salary;
+  // 	console.log(employeeName, startYear, salary, role);
+  // })
+timeDiff = function(start,end) {
+  	return moment(new Date(end)).diff(new Date(start), 'months', true);
+};
+$("#btn-submit").on("click", function(event){
+
+	event.preventDefault();
+	today = moment().format("DD-MM-YYYY")
+	startYear = $("#start-year").val().trim();
+	timeBetween = Math.floor(timeDiff(startYear, today));
+	employeeName = $("#employee-name").val().trim();
+	role = $("#employee-role").val().trim();
+	salary = $("#salary").val().trim();
+	totalSalary = timeBetween * salary;
+
+	database.ref().push({
+		name: employeeName,
+		role: role,
+		year: startYear,
+		salary: salary,
+		total: totalSalary,
+
+	});
 
 console.log(employeeName, startYear, salary);
 
 
-$("#employee-table").append("<tr><td id='name'>" + employeeName + "</td>" + "<td id='role'>" + role + "</td>" + "<td id='year'>" + startYear + "</td>" + "<td id=>" + salary + "</td></tr>");
-
-})
-
+$("#employee-table").append("<tr><td>" + "<strong>Name:</strong> " + employeeName + 
+	"</td>" + "<td>" + "<strong>Role:</strong> " + role + "</td>" + "<td>" + "<strong>Start Date:</strong> " 
+	+ startYear + "</td>" + "<td>" + "<strong>Salary:</strong> " + salary + 
+	"</td>" + "<td>" + "<strong>Total earnings:</strong> " + totalSalary + "</td></tr>");
+});
